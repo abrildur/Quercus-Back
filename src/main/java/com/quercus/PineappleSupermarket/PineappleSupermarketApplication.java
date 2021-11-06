4c8b59704214d98c0ec950f21a891b6d9a4e8d07
AylenG <aylen.p.gomez@gmail.com>
2021-11-06 00:03:51 -0300

    Primer commit

---
+++ src/main/java/com/quercus/PineappleSupermarket/PineappleSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.quercus.PineappleSupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineappleSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineappleSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quercus/PineappleSupermarket/PineappleSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quercus.PineappleSupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineappleSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
